0fd9ab078a95f9d86e2e1b4bd2185adae63e0512
trinoska <trinoska@users.noreply.github.com>
2018-05-08 12:41:53 +0700

    Use stable sort

--- 9-sort.js
+++ 9-sort.js
@@ -3,8 +3,19 @@ function getSortedArr(arrStr){
     var arrInds = arr.map(function(v, i) {
         return [i + 1, parseInt(v)];
     });
-    arrInds.sort(function(n1, n2){return n1[1] - n2[1]});
+    arrInds = selectionSort(arrInds);
     return arrInds;
+}
+
+function selectionSort(arr){
+    var n = arr.length;
+    for (var i = 0; i < n-1; i++)
+    { var min = i;
+        for (var j = i+1; j < n; j++)
+        { if (arr[j][1] < arr[min][1]) min = j; }
+        var t = arr[min]; arr[min] = arr[i]; arr[i] = t;
+    }
+    return arr;
 }
 
 function formTable(arrInds, el){
