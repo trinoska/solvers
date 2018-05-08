fbaccd16cf532fe6089dbff52a4b981199e08d8e
trinoska <trinoska@users.noreply.github.com>
2018-05-08 20:21:48 +0700

    C/D switch for RSA signature

--- 7-rsa.js
+++ 7-rsa.js
@@ -1,14 +1,25 @@
-function calc(p, q, d, m, el){
+function calc(p, q, cOrD, m, el){
     var resStr = '';
     var n = p * q;
     resStr += '<div> N = p * q = ' + p + ' * ' + q + ' = ' + n + '</div>';
     var f = (p - 1) * (q - 1);
     resStr += '<div> F = (p - 1) * (q - 1) = ' + (p - 1) + ' * ' + (q - 1) + ' = ' + f + '</div>';
-    resStr += '<div> C = d ^ (-1) mod F = ' + d + ' ^ (-1) mod ' + f + '</div>';
-    var invC = findInverse(d, f);
-    var c = invC.num;
-    resStr += invC.str;
-    resStr += '<div> C = ' + invC.num + '</div>';
+    var c, d;
+    if(cOrD.name === 'D') {
+        d = cOrD.value;
+        resStr += '<div> C = d ^ (-1) mod F = ' + d + ' ^ (-1) mod ' + f + '</div>';
+        var invC = findInverse(d, f);
+        c = invC.num;
+        resStr += invC.str;
+        resStr += '<div> C = ' + c + '</div>';
+    } else {
+        c = cOrD.value;
+        resStr += '<div> D = c ^ (-1) mod F = ' + c + ' ^ (-1) mod ' + f + '</div>';
+        var invD = findInverse(c, f);
+        d = invD.num;
+        resStr += invD.str;
+        resStr += '<div> D = ' + d + '</div>';
+    }
     resStr += '<div> s = m ^ C mod N = ' + m + ' ^ ' + c + ' mod ' + n + '</div>';
     var powRes = fastPow(m, c, n);
     resStr += powRes.str;
@@ -24,7 +35,7 @@ window.onload = function() { document.getElementById('doOp').onclick = function(
     calc(
         document.getElementById('PVal').value,
         document.getElementById('QVal').value,
-        document.getElementById('DVal').value,
+        getTextRadio('COrD', 'COrDVal'),
         document.getElementById('MVal').value,
         document.getElementById('result')
     );
